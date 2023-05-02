2d299b12b538068357502f94577179d453667fe5
Cyphound <cyphound@gmail.com>
2023-05-01 22:52:50 -0400

    update

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<h1>Hello World</h1>)
+
+function Greeting() {
+    const name = "Cyphound";
+
+    return <h1>{name}</h1>
+}
+
+root.render(
+    <Greeting/>
+)
